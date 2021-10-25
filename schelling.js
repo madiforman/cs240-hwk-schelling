88d8173934344ca633d7e19d357cbb0a34b0394c
Madison Sanchez-Forman <msanchezforman@pugetsound.edu>
2021-10-25 11:40:18 -0700

    generic table of h x w dimension created with javascript, not functional

--- schelling.js
+++ schelling.js
@@ -1,4 +1,39 @@
 /**
  * Schelling's Model simulator
- * @author
+ * @ Madison Sanchez-Forman
  */
+ 
+ var dimension = document.querySelector("#dimension");
+ var threshold = document.querySelector("#threshold");
+ var vacant = document.querySelector("#vacantRatio");
+ var split = document.querySelector("#popRatio");
+
+let size = dimension.value;
+function intiArr(){
+    board = [];
+    for(var i = 0; i < size; i++){
+        board[i] = [];
+        for(var j = 0; j < size; j++){
+            board[i][j]=-1
+        }
+    }
+    return board;
+}
+let arr = intiArr()
+function makeTableTwo(array) {
+    var table = document.createElement('table');
+    for (var i = 0; i < array.length; i++) {
+        var row = document.createElement('tr');
+        row.style.backgroundColor = "#ffff"
+        for (var j = 0; j < array[i].length; j++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell);
+            cell.style.backgroundColor = "#ffff"
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
+let table = document.getElementById("board");
+let t = makeTableTwo(arr);
+document.getElementById("board").outerHTML = t.outerHTML;
