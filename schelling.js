6e5761b8f076fb1392ac8fe9f187877142cf188a
Madison Sanchez-Forman <msanchezforman@pugetsound.edu>
2021-10-28 11:23:02 -0700

    got size of grid to change dynamically

--- schelling.js
+++ schelling.js
@@ -3,24 +3,17 @@
  * @ Madison Sanchez-Forman
  */
  
- var dimension = document.querySelector("#dimension");
- var threshold = document.querySelector("#threshold");
- var vacant = document.querySelector("#vacantRatio");
- var split = document.querySelector("#popRatio");
+let table = document.getElementById("board");
+var dimension = document.querySelector("#dimension");
+table.appendChild(makeTable(dimension.value));
+dimension.addEventListener("input", function(){
+    table.removeChild(table.firstElementChild);
+    var newSize = dimension.innerHTML;
+    table.appendChild(makeTable(dimension.value));
 
-let size = dimension.value;
-function intiArr(){
-    board = [];
-    for(var i = 0; i < size; i++){
-        board[i] = [];
-        for(var j = 0; j < size; j++){
-            board[i][j]=-1
-        }
-    }
-    return board;
-}
-let arr = intiArr()
-function makeTableTwo(array) {
+})
+function makeTable() {
+    let array = intiArr(dimension.value)
     var table = document.createElement('table');
     for (var i = 0; i < array.length; i++) {
         var row = document.createElement('tr');
@@ -34,6 +27,14 @@ function makeTableTwo(array) {
     }
     return table;
 }
-let table = document.getElementById("board");
-let t = makeTableTwo(arr);
-document.getElementById("board").outerHTML = t.outerHTML;
+
+function intiArr(size){
+     size = dimension.value;
+    board = [];
+    for(var i = 0; i < size; i++){
+        board[i] = [];
+        for(var j = 0; j < size; j++){
+            board[i][j]=-1 }
+         }
+    return board; 
+}
